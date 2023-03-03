de67026e037b547383650ab40ca2e52d27bf7534
ITSunbank <sebastian.vallejo@sunbank.mx>
2023-03-02 18:33:11 -0600

    [Update] Creación de estructura

---
+++ Tarea4/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+//const routes = require('./src/rutas');
+const rutas = require('./rutas'); 
+const axios = require("axios");
+
+const {engine} = require('express-handlebars');
+
+
+const app = express();
+
+app.engine('handlebars',engine());
+app.set('view engine','handlebars');
+app.set('views','./src/views');
+
+
+
+const port = 3000;
+//app.use('', routes);
+rutas(app);
+
+app.listen(port,function() {
+    console.log('app is running in port ' + port)
+});
